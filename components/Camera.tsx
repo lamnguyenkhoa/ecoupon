2fddb6e69aad478a31c9d679de986ee71b70ada8
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2022-04-22 12:55:34 +1000

    Align camera button

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -1,31 +1,44 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Animated } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Animated,
+  Dimensions,
+} from "react-native";
+import { Camera } from "expo-camera";
 
 export function ChallengeCamera() {
   const [hasPermission, setHasPermission] = useState(null);
-  const cameraRef = useRef(null)
-  const fadeAnim = useRef(new Animated.Value(1)).current
+  const cameraRef = useRef(null);
+  const fadeAnim = useRef(new Animated.Value(1)).current;
 
   const shutterEffect = () => {
     Animated.sequence([
       // @ts-ignore
-      Animated.timing(fadeAnim, { toValue: 0, duration: 150, useNativeDriver: true }),
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 150,
+        useNativeDriver: true,
+      }),
       // @ts-ignore
-      Animated.timing(fadeAnim, { toValue: 1, duration: 100, useNativeDriver: true })
-    ]).start()
-  }
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
 
   useEffect(() => {
-    // Request permission 
+    // Request permission
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
       // @ts-ignore
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
-
-
 
   if (hasPermission === null) {
     return <View />;
@@ -37,24 +50,22 @@ export function ChallengeCamera() {
     // @ts-ignore
     <Animated.View style={[styles.effectcontainer, { opacity: fadeAnim }]}>
       <Camera style={styles.camera} ref={cameraRef}>
-
         {/* Snap button */}
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.button}
             onPress={async () => {
               if (cameraRef.current) {
-                // @ts-ignore 44147937 
+                // @ts-ignore 44147937
                 let photo = await cameraRef.current.takePictureAsync();
-                console.log(photo)
-                shutterEffect()
+                console.log(photo);
+                shutterEffect();
               }
-            }
-            }>
+            }}
+          >
             <Text style={styles.text}> Snap </Text>
           </TouchableOpacity>
         </View>
-
       </Camera>
     </Animated.View>
   );
@@ -62,24 +73,32 @@ export function ChallengeCamera() {
 
 const styles = StyleSheet.create({
   effectcontainer: {
-    flex: 1
+    flex: 1,
   },
   camera: {
     flex: 1,
   },
   buttonContainer: {
     flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
+    backgroundColor: "transparent",
+    flexDirection: "row",
   },
   button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    marginBottom: 20,
+    borderRadius:
+      Math.round(
+        Dimensions.get("window").width + Dimensions.get("window").height
+      ) / 2,
+    width: 120,
+    height: 120,
+    backgroundColor: "#f00",
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center",
+    left: Dimensions.get("window").width / 2 - 60,
   },
   text: {
     fontSize: 18,
-    color: 'white',
+    color: "white",
   },
 });
